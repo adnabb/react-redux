ff74fe6947e39ceace1e7c8ad7388c3f48a19683
chenshiyu <chenshiyu@ygsoft.com>
2020-07-03 14:36:04 +0800

    feat:V1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const store = props.store;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{store.getState().num}</span>
+      <button onClick={() => { add1(store); }}>+1</button>
+      <button onClick={() => { add2(store); }}>+2</button>
+      <button onClick={() => { addOdd(store); }}>单数+1</button>
+      <button onClick={() => { addAfter2Sec(store); }}>异步两秒后+1</button>
     </div>
   );
+}
+
+function add1(store) {
+  store.dispatch({
+    type: 'add',
+    payload: 1
+  });
+}
+
+
+function add2(store) {
+  store.dispatch({
+    type: 'add',
+    payload: 2
+  });
+}
+
+
+function addOdd(store) {
+  if (store.getState().num % 2 === 1) {
+    store.dispatch({
+      type: 'add',
+      payload: 1
+    });
+  }
+}
+
+function addAfter2Sec(store) {
+  setTimeout(() => {
+    store.dispatch({
+      type: 'add',
+      payload: 1
+    });
+  }, 2000);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function reducer(state, action) {
+  if (typeof state === 'undefined') return {
+    num: 0
+  };
+
+  if (action.type === 'add') return {
+    num: state.num + action.payload
+  }
+
+  return state;
+}
+const store = createStore(reducer);
+function render () {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App store={store}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render()
+
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
